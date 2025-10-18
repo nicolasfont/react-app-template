2e47539c969320cb56f3c37117df1c219da0e231
Nicolás Font <nicolasfont@gmail.com>
2025-10-18 16:28:46 -0300

    Add Radix UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,13 @@
-import { useState } from "react";
-import viteLogo from "/vite.svg";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { Flex, Heading, Text, Theme } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noopener">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noopener">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Theme accentColor="green">
+      <Flex direction="column" gap="2">
+        <Heading>react-app-template</Heading>
+        <Text>A wonderful application template.</Text>
+      </Flex>
+    </Theme>
   );
 }
-
-export default App;
